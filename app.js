7d70715283dbccf282fcdef0a0d45ad4f7bc00b1
glushakova <glushakovanastya@gmail.com>
2020-02-05 16:56:21 +0300

    one

---
+++ app.js
@@ -0,0 +1,27 @@
+var student1 = {
+  lastName: "Inanov",
+  age: 15,
+  gender: "female",
+  awards: true
+};
+var student2 = {
+  lastName: "Petrova",
+  age: 21,
+  gender: "male",
+  awards: false
+};
+var student3 = {
+  lastName: "Koroleva",
+  age: 17,
+  gender: "male",
+  awards: false
+};
+var student4 = {
+  lastName: "Sidorov",
+  age: 27,
+  gender: "female",
+  awards: false
+};
+
+var arr = [student1, student2, student3, student4];
+console.log(arr);
